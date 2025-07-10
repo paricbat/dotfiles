e0ff3003f53b78bbb0852c6553e2cfd0b8fbad09
Veronika Bušová <paricbat@projectspink.eu>
2025-07-10 02:50:46 +0200

    remove firefox user.js and update_systen.fish

--- update_system.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/fish
-
-rpm-ostree upgrade
-flatpak update -y
-
-distrobox enter fedora-dbox -- deno upgrade
-distrobox enter fedora-dbox -- sudo dnf update -y
-distrobox enter fedora-dbox -- rustup update stable
